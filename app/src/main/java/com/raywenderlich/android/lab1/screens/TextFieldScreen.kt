50068309e9e974bb03a535f4d1784d5649c1f5bd
Konst <konst.partnership@gmail.com>
2024-03-01 09:22:21 +0300

    Изменения в TextFieldScreen.kt: Добавление TextField

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
@@ -3,9 +3,13 @@ package com.raywenderlich.android.lab1.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -25,7 +29,14 @@ fun TextFieldScreen() {
     }
 }
 
+@Preview(showSystemUi = true)
 @Composable
 fun MyTextField() {
-    //TODO add your code here
+    val  textValue = remember { mutableStateOf("") }
+
+    TextField(
+        value = textValue.value,
+        onValueChange = {textValue.value = it},
+        label = {}
+    )
 }
